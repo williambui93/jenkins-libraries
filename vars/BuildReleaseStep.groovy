67c1aaf2e6797995b80a37a899c393b0b27b8d19
williambui93 <132336999+williambui93@users.noreply.github.com>
2023-05-15 18:03:41 +0700

    Update BuildReleaseStep.groovy

--- vars/BuildReleaseStep.groovy
+++ vars/BuildReleaseStep.groovy
@@ -12,8 +12,8 @@
 def call(Map config = [:]) {
     if (isUnix()) {
         if (config.executableName) {
-            def optionalParams = config.skipBuildEvent ? "/p:PreBuildEvent=;PostBuildEvent=" : ""
-            sh "DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1 $DOTNET/dotnet build AdIns.Filing.DataModel.csproj /p:PreBuildEvent=''"
+            def optionalParams = config.skipBuildEvent ? "/p:PreBuildEvent=" : ""
+            sh "DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1 $DOTNET/dotnet build -c Release /p:PreBuildEvent="
             sh "DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1 $DOTNET/dotnet publish -c Release --output ./publish/release /p:PreBuildEvent=''"
             configFileProvider([configFile(fileId: config.dockerfile ? config.dockerfile: 'dockerfile-be', targetLocation: 'publish/release/Dockerfile', variable: 'dockerfile'), configFile(fileId: 'swagger-xml', targetLocation: "publish/release/${config.executableName}.xml", variable: 'swagger')]) {
                 sh "chmod 0777 publish/release/Dockerfile"
