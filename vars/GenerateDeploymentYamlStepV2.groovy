1b4d0a8f8a9470672fcc7a33865922c4be0d5156
williambui93 <132336999+williambui93@users.noreply.github.com>
2023-07-14 14:31:37 +0700

    Update GenerateDeploymentYamlStepV2.groovy
    auto chage general setting

--- vars/GenerateDeploymentYamlStepV2.groovy
+++ vars/GenerateDeploymentYamlStepV2.groovy
@@ -24,7 +24,7 @@ import org.yaml.snakeyaml.Yaml
 
 def call(Map config = [:]) {
     if (isUnix()) {
-            configFileProvider([configFile(fileId: 'kube-deployment-yaml', targetLocation: './deployment.yaml', variable: 'deployment'), configFile(fileId: 'kube-service-yaml', targetLocation: './service.yaml', variable: 'service'), configFile(fileId: 'kube-configmap-yaml', targetLocation: './configmap.yaml', variable: 'configmap')]) {
+            configFileProvider([configFile(fileId: 'kube-deployment-yaml', targetLocation: './deployment.yaml', variable: 'deployment'), configFile(fileId: 'kube-service-yaml', targetLocation: './service.yaml', variable: 'service'), configFile(fileId: 'kube-configmap-yaml', targetLocation: './configmap.yaml', variable: 'configmap'), configFile(fileId: 'GeneralConfig', targetLocation: './GeneralConfig.json', variable: 'GeneralConfig')]) {
             def matchers = ~ /.*-(frontend|fe)/
             config.type = config.type ? config.type: (matchers.matcher(config.deploymentName).matches() ? "fe": "be")
 
@@ -69,6 +69,303 @@ def call(Map config = [:]) {
             configmap.metadata.name = """${config.deploymentName}-appsettings"""
             configmap.metadata.namespace = config.namespace
             def data = config.configPath ? readFile(config.configPath): "{}"
+
+            if (config.type == 'fe')
+            {
+                def jsonString = data
+                def jsonSetting = readFile(file: 'GeneralConfig.json')
+                
+                // Membaca JSON
+                jsonString = jsonString.replace("\\", "/")
+                
+                //hapus comment di json
+                def jsonStringWithoutComments = jsonString.replaceAll(/\/\*(?:[^*]|(?:\*+[^*\/]))*\*\//, '')
+                
+                def jsonAppSetting = new JsonSlurper().parseText(jsonStringWithoutComments)
+                def jsonConfSetting = new JsonSlurper().parseText(jsonSetting)
+                
+                //Ubah Logging
+                if("Logging" in jsonAppSetting."ConnectionStrings".keySet())
+                {
+                    if(jsonConfSetting."Logging"."DataBaseType" == "POSTGRESQL")
+                    {
+                        def DBString = jsonAppSetting."ConnectionStrings"."Logging"."DataBasePostgreSQL"
+                            
+                        def DBStringSplitData = DBString.split(';')
+                        def keyValuePairs = [:]
+                        
+                        DBStringSplitData.each { pair ->
+                            def keyValue = pair.split('=')
+                            def key = keyValue[0].trim()
+                            def value = keyValue[1].trim()
+                            keyValuePairs[key] = value
+                        }
+                        
+                        keyValuePairs['Host'] = jsonConfSetting."Logging"."DataBasePostgreSQL"."Host"
+                        keyValuePairs['User ID'] = jsonConfSetting."Logging"."DataBasePostgreSQL"."User ID"
+                        keyValuePairs['Password'] = jsonConfSetting."Logging"."DataBasePostgreSQL"."Password"
+                        keyValuePairs['Port'] = jsonConfSetting."Logging"."DataBasePostgreSQL"."Port"
+                        keyValuePairs['Database'] = jsonConfSetting."Logging"."DataBasePostgreSQL"."DatabaseName"
+                        
+                        
+                        def data = keyValuePairs.collect { key, value -> "$key=$value" }.join(';')
+                        
+                        jsonAppSetting."ConnectionStrings"."Logging"."DataBaseType" = jsonConfSetting."Logging"."DataBaseType"
+                        jsonAppSetting."ConnectionStrings"."Logging"."DataBasePostgreSQL" = data
+                    }
+                    else if(jsonConfSetting."Logging"."DataBaseType" == "SSMS")
+                    {
+                        def DBString = jsonAppSetting."ConnectionStrings"."Logging"."DataBaseSSMS"
+                            
+                        def DBStringSplitData = DBString.split(';')
+                        def keyValuePairs = [:]
+                        
+                        DBStringSplitData.each { pair ->
+                            def keyValue = pair.split('=')
+                            def key = keyValue[0].trim()
+                            def value = keyValue[1].trim()
+                            keyValuePairs[key] = value
+                        }
+                        
+                        keyValuePairs['Server'] = jsonConfSetting."Logging"."DataBasePostgreSQL"."Server"
+                        keyValuePairs['User ID'] = jsonConfSetting."Logging"."DataBasePostgreSQL"."User ID"
+                        keyValuePairs['Password'] = jsonConfSetting."Logging"."DataBasePostgreSQL"."Password"
+                        keyValuePairs['Database'] = jsonConfSetting."Logging"."DataBasePostgreSQL"."DatabaseName"
+                        
+                        
+                        def data = keyValuePairs.collect { key, value -> "$key=$value" }.join(';')
+                        
+                        jsonAppSetting."ConnectionStrings"."Logging"."DataBaseType" = jsonConfSetting."Logging"."DataBaseType"
+                        jsonAppSetting."ConnectionStrings"."Logging"."DataBaseSSMS" = data
+                    }
+                }
+                
+                
+                // Ubah Database
+                for(DatabaseName in jsonAppSetting."ConnectionStrings") {
+                 targetField = DatabaseName.key;
+                 if (jsonConfSetting."Database"."DataBaseType" == "POSTGRESQL")
+                 {
+                     if (DatabaseName.key in jsonConfSetting."Database"."DataBasePostgreSQL"."DatabaseName".keySet())
+                     {
+                        for(itemsSetting in jsonAppSetting."ConnectionStrings"."$targetField")
+                        {
+                            if(itemsSetting.key == "DataBasePostgreSQL")
+                            {
+                                def DBString = itemsSetting.value
+                                
+                                def DBStringSplitData = DBString.split(';')
+                                def keyValuePairs = [:]
+                                
+                                DBStringSplitData.each { pair ->
+                                    def keyValue = pair.split('=')
+                                    def key = keyValue[0].trim()
+                                    def value = keyValue[1].trim()
+                                    keyValuePairs[key] = value
+                                }
+                                
+                                keyValuePairs['Host'] = jsonConfSetting."Database"."DataBasePostgreSQL"."Host"
+                                keyValuePairs['User ID'] = jsonConfSetting."Database"."DataBasePostgreSQL"."User ID"
+                                keyValuePairs['Port'] = jsonConfSetting."Database"."DataBasePostgreSQL"."Port"
+                                keyValuePairs['Password'] = jsonConfSetting."Database"."DataBasePostgreSQL"."Password"
+                                keyValuePairs['Database'] = jsonConfSetting."Database"."DataBasePostgreSQL"."DatabaseName"."$targetField"
+                                
+                                
+                                def data = keyValuePairs.collect { key, value -> "$key=$value" }.join(';')
+                                
+                                jsonAppSetting."ConnectionStrings"."$targetField"."DataBasePostgreSQL" = data
+                                
+                                
+                            }
+                            else if(itemsSetting.key == "SQLConnSP")
+                            {
+                                def DBString = itemsSetting.value
+                                
+                                def DBStringSplitData = DBString.split(';')
+                                def keyValuePairs = [:]
+                                
+                                DBStringSplitData.each { pair ->
+                                    def keyValue = pair.split('=')
+                                    def key = keyValue[0].trim()
+                                    def value = keyValue[1].trim()
+                                    keyValuePairs[key] = value
+                                }
+                                
+                                keyValuePairs['Data Source'] = jsonConfSetting."Database"."DataBasePostgreSQL"."Server"
+                                keyValuePairs['user id'] = jsonConfSetting."Database"."DataBasePostgreSQL"."User ID"
+                                keyValuePairs['Password'] = jsonConfSetting."Database"."DataBasePostgreSQL"."Password"
+                                keyValuePairs['Initial Catalog'] = jsonConfSetting."Database"."DataBasePostgreSQL"."DatabaseName"."$targetField"
+                                
+                                
+                                def data = keyValuePairs.collect { key, value -> "$key=$value" }.join(';')
+                                
+                                jsonAppSetting."ConnectionStrings"."$targetField"."SQLConnSP" = data
+                                
+                            }
+                        }
+                     
+                    }
+                 
+                 }
+                 else if (jsonConfSetting."Database"."DataBaseType" == "SSMS")
+                 {
+                     if('DataBaseSSMS' in jsonAppSetting."ConnectionStrings"."$targetField")
+                     {
+                         if (DatabaseName.key in jsonConfSetting."Database"."DataBaseSSMS"."DatabaseName".keySet())
+                         {
+                            for(itemsSetting in jsonAppSetting."ConnectionStrings"."$targetField")
+                            {
+                                if(itemsSetting.key == 'DataBaseSSMS')
+                                {
+                                    def DBString = itemsSetting.value
+                                    
+                                    def DBStringSplitData = DBString.split(';')
+                                    def keyValuePairs = [:]
+                                    
+                                    DBStringSplitData.each { pair ->
+                                        def keyValue = pair.split('=')
+                                        def key = keyValue[0].trim()
+                                        def value = keyValue[1].trim()
+                                        keyValuePairs[key] = value
+                                    }
+                                    
+                                    keyValuePairs['Server'] = jsonConfSetting."Database"."DataBaseSSMS"."Server"
+                                    keyValuePairs['User ID'] = jsonConfSetting."Database"."DataBaseSSMS"."User ID"
+                                    keyValuePairs['Password'] = jsonConfSetting."Database"."DataBaseSSMS"."Password"
+                                    keyValuePairs['Database'] = jsonConfSetting."Database"."DataBaseSSMS"."DatabaseName"."$targetField"
+                                    
+                                    
+                                    def data = keyValuePairs.collect { key, value -> "$key=$value" }.join(';')
+                                    
+                                    jsonAppSetting."ConnectionStrings"."$targetField"."DataBaseSSMS" = data
+                                    
+                                    
+                                }
+                                else if(itemsSetting.key == 'SQLConnSP')
+                                {
+                                    def DBString = itemsSetting.value
+                                    
+                                    def DBStringSplitData = DBString.split(';')
+                                    def keyValuePairs = [:]
+                                    
+                                    DBStringSplitData.each { pair ->
+                                        def keyValue = pair.split('=')
+                                        def key = keyValue[0].trim()
+                                        def value = keyValue[1].trim()
+                                        keyValuePairs[key] = value
+                                    }
+                                    
+                                    keyValuePairs['Data Source'] = jsonConfSetting."Database"."DataBaseSSMS"."Server"
+                                    keyValuePairs['user id'] = jsonConfSetting."Database"."DataBaseSSMS"."User ID"
+                                    keyValuePairs['Password'] = jsonConfSetting."Database"."DataBaseSSMS"."Password"
+                                    keyValuePairs['Initial Catalog'] = jsonConfSetting."Database"."DataBaseSSMS"."DatabaseName"."$targetField"
+                                    
+                                    
+                                    def data = keyValuePairs.collect { key, value -> "$key=$value" }.join(';')
+                                    
+                                    jsonAppSetting."ConnectionStrings"."$targetField"."SQLConnSP" = data
+                                    
+                                }
+                            }
+                         
+                        }
+                     }
+                     else
+                     {
+                         if (DatabaseName.key in jsonConfSetting."Database"."DataBaseSSMS"."DatabaseName".keySet())
+                         {
+                            for(itemsSetting in jsonAppSetting."ConnectionStrings"."$targetField")
+                            {
+                                if(itemsSetting.key == 'DataBase')
+                                {
+                                    def DBString = itemsSetting.value
+                                    
+                                    def DBStringSplitData = DBString.split(';')
+                                    def keyValuePairs = [:]
+                                    
+                                    DBStringSplitData.each { pair ->
+                                        def keyValue = pair.split('=')
+                                        def key = keyValue[0].trim()
+                                        def value = keyValue[1].trim()
+                                        keyValuePairs[key] = value
+                                    }
+                                    
+                                    keyValuePairs['Server'] = jsonConfSetting."Database"."DataBaseSSMS"."Server"
+                                    keyValuePairs['User ID'] = jsonConfSetting."Database"."DataBaseSSMS"."User ID"
+                                    keyValuePairs['Password'] = jsonConfSetting."Database"."DataBaseSSMS"."Password"
+                                    keyValuePairs['Database'] = jsonConfSetting."Database"."DataBaseSSMS"."DatabaseName"."$targetField"
+                                    
+                                    
+                                    def data = keyValuePairs.collect { key, value -> "$key=$value" }.join(';')
+                                    
+                                    jsonAppSetting."ConnectionStrings"."$targetField"."DataBase" = data
+                                    
+                                    
+                                }
+                                else if(itemsSetting.key == 'SQLConnSP')
+                                {
+                                    def DBString = itemsSetting.value
+                                    
+                                    def DBStringSplitData = DBString.split(';')
+                                    def keyValuePairs = [:]
+                                    
+                                    DBStringSplitData.each { pair ->
+                                        def keyValue = pair.split('=')
+                                        def key = keyValue[0].trim()
+                                        def value = keyValue[1].trim()
+                                        keyValuePairs[key] = value
+                                    }
+                                    
+                                    keyValuePairs['Data Source'] = jsonConfSetting."Database"."DataBaseSSMS"."Server"
+                                    keyValuePairs['user id'] = jsonConfSetting."Database"."DataBaseSSMS"."User ID"
+                                    keyValuePairs['Password'] = jsonConfSetting."Database"."DataBaseSSMS"."Password"
+                                    keyValuePairs['Initial Catalog'] = jsonConfSetting."Database"."DataBaseSSMS"."DatabaseName"."$targetField"
+                                    
+                                    
+                                    def data = keyValuePairs.collect { key, value -> "$key=$value" }.join(';')
+                                    
+                                    jsonAppSetting."ConnectionStrings"."$targetField"."SQLConnSP" = data
+                                    
+                                }
+                            }
+                         
+                        }
+                     }
+                 
+                 }
+                 
+                }
+                
+                
+                //Ubah URL
+                for(BeUrlName in jsonAppSetting."ConnectionStrings") {
+                 if (BeUrlName.key in jsonConfSetting."URL".keySet())
+                 {                     
+                    targetField = BeUrlName.key;
+                     
+                    for(itemsSetting in jsonAppSetting."ConnectionStrings"."$targetField")
+                    {
+                        if(itemsSetting.key == 'URL')
+                        {
+                            def UrlString = jsonConfSetting."URL"."$targetField"
+                            jsonAppSetting."ConnectionStrings"."$targetField"."URL" = UrlString
+                        }
+                    }
+                     
+                 }
+                }
+                
+                
+                //Ubah Connection String Redis
+                jsonAppSetting."RedisConfig"."ConnStringRedis" = jsonConfSetting."Redis"."ConnStringRedis"
+                
+                // Memperbarui nilai field tertentu
+                //json."test"."LogLevel"."$targetField" = newValue
+                
+                // Mengubah JSON kembali menjadi string
+                data = JsonOutput.toJson(jsonAppSetting)
+            }
+            
             Map configData = [(config.configMapFileName): data]
             configmap.data = configData
 
